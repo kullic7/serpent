f0c552e1b149f306f32d26b074adce2a39fb2a49
rado.fratrik <r.fratrik7@gmail.com>
2025-12-23 11:08:47 +0100

    add commons and config

---
+++ common/config.h
@@ -0,0 +1,35 @@
+#ifndef SERPENT_CONFIG_H
+#define SERPENT_CONFIG_H
+
+#define MAX_CLIENTS 16
+#define TICK_RATE 10  // Game updates per second
+#define INITIAL_SNAKE_LENGTH 3
+#define FRUIT_COUNT 5
+#define MAX_EVENTS 1024
+
+#define MENU_MAX_TEXT_LENGTH 64
+
+#define BUTTON_WIDTH 40
+#define BUTTON_HEIGHT 10
+
+
+#define TARGET_FPS 60
+#define FRAME_TIME_MS (1000 / TARGET_FPS)
+
+#define SNAKE_CHAR "o"
+#define FRUIT_CHAR "*"
+#define OBSTACLE_CHAR "#"
+
+// ascii key codes
+#define KEY_DOWN    's'
+#define KEY_UP      'w'
+#define KEY_LEFT    'a'
+#define KEY_RIGHT   'd'
+#define KEY_PAUSE   'p'
+#define KEY_QUIT    'q'
+#define KEY_ENTER   '\n'
+#define KEY_ESC     '\x1b'
+
+#define MENU_STACK_MAX 8
+
+#endif //SERPENT_CONFIG_H
---
+++ common/game_types.h
@@ -0,0 +1,68 @@
+#ifndef SERPENT_GAME_TYPES_H
+#define SERPENT_GAME_TYPES_H
+
+#include <stdbool.h>
+#include "common/config.h"
+
+typedef struct {
+    int x;
+    int y;
+} Position;
+
+typedef struct {
+    int width;
+    int height;
+} Size;
+
+typedef struct {
+    Size size;
+    Position pos;
+} Box;
+
+typedef struct {
+    Box box; // TODO probably simplification needed
+
+    const char text[MENU_MAX_TEXT_LENGTH];
+
+} TextField;
+
+typedef enum {
+    DIR_UP,
+    DIR_DOWN,
+    DIR_LEFT,
+    DIR_RIGHT
+} Direction;
+
+typedef struct {
+    Position *body; // dynamic array of positions
+    size_t length;
+} RenderSnake;
+
+
+typedef struct {
+    Position *body; // dynamic array of positions
+    size_t length;
+    size_t capacity;
+    Direction direction;     // UP, DOWN, LEFT, RIGHT
+    Direction next_direction;
+    bool alive; // maybe it should be active instead
+} Snake;
+
+typedef struct {
+    int id;
+    Snake snake;
+    size_t score;
+} Player;
+
+typedef struct {
+    Position pos;
+    bool active;
+} Fruit;
+
+typedef struct {
+    Position pos;
+} Obstacle;
+
+
+
+#endif //SERPENT_GAME_TYPES_H
